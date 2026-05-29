b1412e82625a913a31acc4a3730e22786f2442d8
4npweb <4np.web@gmail.com>
2026-05-30 00:12:19 +0200

    Added switching screens

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
+//Service worker registration
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('sw.js')
             .then(reg => console.log('Service Worker registered completely!', reg))
             .catch(err => console.error('Service Worker registration failed', err));
     });
+}
+
+//Variables
+const homeScreen = document.getElementById("home");
+const gradesScreen = document.getElementById("grades");
+const timetableScreen = document.getElementById("timetable");
+const eventsScreen = document.getElementById("events");
+
+goHome()
+
+function goHome() {
+    homeScreen.classList.add("show");
+    gradesScreen.classList.remove("show");
+    timetableScreen.classList.remove("show");
+    eventsScreen.classList.remove("show");
+}
+
+function goGrades() {
+    homeScreen.classList.remove("show");
+    gradesScreen.classList.add("show");
+    timetableScreen.classList.remove("show");
+    eventsScreen.classList.remove("show");
+}
+
+function goTimetable() {
+    homeScreen.classList.remove("show");
+    gradesScreen.classList.remove("show");
+    timetableScreen.classList.add("show");
+    eventsScreen.classList.remove("show");
+}
+
+function goEvents() {
+    homeScreen.classList.remove("show");
+    gradesScreen.classList.remove("show");
+    timetableScreen.classList.remove("show");
+    eventsScreen.classList.add("show");
 }
