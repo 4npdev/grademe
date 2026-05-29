e2d88c6ef0a52a6eb1eaf1dcc4273b22aac68fc0
4npweb <4np.web@gmail.com>
2026-05-29 20:07:16 +0200

    sw.js fix attempt

--- sw.js
+++ sw.js
@@ -1,24 +1,35 @@
 const CACHE_NAME = 'grademe-v1';
 const ASSETS = [
-  'index.html',
-  'index.css',
-  'css/home.css',
-  'css/grades.css',
-  'js/storage.js',
-  'app.js',
-  'manifest.json'
+  './',
+  './index.html',
+  './index.css',
+  './css/home.css',
+  './css/grades.css',
+  './js/storage.js',
+  './app.js',
+  './manifest.json',
+  './assets/GradeMe.png'
 ];
-
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(e.request).catch(() => {
+        if (e.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
+    })
   );
 });
